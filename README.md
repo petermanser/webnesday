50747a869856852fdf27d9ade200c48e90e03cd6
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2014-02-02 19:25:01 +0100

    update tests, update readme

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-webnesday
+Webnesday.ch
 =========
 
 Webnesday Website
+
+#Run Tests
+
+##Install Karma
+    npm install karma
+    npm install karma-junit-reporter
+    npm install karma-ng-scenario
+
+
+##Start Webserver 
+    node scripts/web-server.js
+
+
+##Run Tests
+    scripts/e2e-test.sh
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -5,7 +5,7 @@
 describe('my app', function() {
 
   beforeEach(function() {
-    browser().navigateTo('/webnesday/app/index.html');
+    browser().navigateTo('/app/index.html');
   });
 
 
@@ -19,21 +19,40 @@ describe('my app', function() {
     it('should render signup when user navigates to /', function() {
       expect(element('h1').text()).
         toMatch("Sign up for further information.");
+      expect(element('.active').text()).
+        toMatch("Sign up");
+    });
+
+  });
+
+  describe('signup click', function() {
+
+    beforeEach(function() {
+      browser().navigateTo('#/');
+    });
+
+    it('should render signup when user navigates to signup', function() {
+      expect(element('h1').text()).
+        toMatch("Sign up for further information.");
+      expect(element('.active').text()).
+        toMatch("Sign up");
     });
 
   });
 
 
-  describe('view2', function() {
+  describe('contact', function() {
 
     beforeEach(function() {
-      browser().navigateTo('/webnesday/app/#/contact');
+      browser().navigateTo('#/contact');
     });
 
 
-    it('should render view2 when user navigates to /view2', function() {
+    it('should render contact when user navigates to contact', function() {
       expect(element('[ng-view] p:first').text()).
         toMatch(/Currently we're looking for companies to host the Webnesday's and to sponsor beer. If your company wants to do either of it, please contact us./);
+      expect(element('.active').text()).
+        toMatch(/Contact/);
     });
 
   });
